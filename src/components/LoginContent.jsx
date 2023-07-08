7619febbd44d493dade4a838de6451c777e087cb
Osama shahbaz <oshahbaz74@gmail.com>
2023-07-09 00:09:14 +0500

    update readme

--- src/components/LoginContent.jsx
+++ src/components/LoginContent.jsx
@@ -1,24 +1,35 @@
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 import styles from './LoginContent.module.css';
 function LoginContent() {
   return (
     <div className={styles.main_con}>
-      <div className={styles.login_con}>
+      <form className={styles.login_con} name="login" netlify>
         <div className={styles.heading_con}>
           <h1>Login here</h1>
         </div>
         <div className={styles.user_input}>
           <label htmlFor="username">Username</label>
-          <input type="text" placeholder="Username" id="username" />
+          <input
+            type="text"
+            placeholder="Username"
+            id="username"
+            name="username"
+          />
         </div>
         <div className={styles.user_Pass}>
           <label htmlFor="password">Username</label>
-          <input type="password" placeholder="password" id="password" />
+          <input
+            type="password"
+            placeholder="password"
+            id="password"
+            name="password"
+          />
         </div>
         <button type="submit" className={styles.login_btn}>
           Login
         </button>
-      </div>
+      </form>
     </div>
   );
 }
--- src/components/SignUpcontent.jsx
+++ src/components/SignUpcontent.jsx
@@ -1,14 +1,20 @@
+/* eslint-disable react/no-unknown-property */
 import styles from './SignUpcontent.module.css';
 function SignUpcontent() {
   return (
     <div className={styles.main_con}>
-      <div className={styles.login_con}>
+      <form name="signup" className={styles.login_con} netlify>
         <div className={styles.heading_con}>
           <h1>Create your account</h1>
         </div>
         <div className={styles.user_input}>
           <label htmlFor="username">Email</label>
-          <input type="email" placeholder="Email" id="username" />
+          <input
+            type="email"
+            placeholder="Email"
+            name="username"
+            id="username"
+          />
         </div>
         {/* <div className={styles.user_Pass}>
           <label htmlFor="password">Username</label>
@@ -17,7 +23,7 @@ function SignUpcontent() {
         <button type="submit" className={styles.login_btn}>
           Create an account
         </button>
-      </div>
+      </form>
     </div>
   );
 }
